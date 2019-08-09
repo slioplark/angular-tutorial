4c25b681b0ce5808007656c570a78a501f1bb206
slioplark <jeailfy9520@outlook.com>
2019-08-09 16:08:29 +0800

    feat startDate & endDate

--- src/app/main/material/datepicker/datepicker.component.ts
+++ src/app/main/material/datepicker/datepicker.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-datepicker',
   templateUrl: './datepicker.component.html',
-  styleUrls: ['./datepicker.component.css']
+  styleUrls: ['./datepicker.component.scss']
 })
 export class DatepickerComponent implements OnInit {
 
-  constructor() { }
+  form = this.formBuilder.group({
+    startDate: [null],
+    endDate: [null]
+  });
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+  }
+
+  onDateChange(event) {
+    console.log(event);
   }
 
 }
