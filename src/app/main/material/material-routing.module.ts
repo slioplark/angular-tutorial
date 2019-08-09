349362bf484e264c35f3759725d1d7b3fcdef950
slioplark <jeailfy9520@outlook.com>
2019-08-09 15:31:09 +0800

    feat material datepicker

--- src/app/main/material/material-routing.module.ts
+++ src/app/main/material/material-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AutocompleteComponent } from './autocomplete/autocomplete.component';
 import { DialogComponent } from './dialog/dialog.component';
 import { TableComponent } from './table/table.component';
+import { DatepickerComponent } from './datepicker/datepicker.component';
 
 const routes: Routes = [
   { path: 'autocomplete', component: AutocompleteComponent },
   { path: 'dialog', component: DialogComponent },
-  { path: 'table', component: TableComponent }
+  { path: 'table', component: TableComponent },
+  { path: 'datepicker', component: DatepickerComponent }
 ];
 
 @NgModule({
--- src/app/main/material/material.module.ts
+++ src/app/main/material/material.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
+import { MAT_DATE_LOCALE } from '@angular/material';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 import { AutocompleteComponent } from './autocomplete/autocomplete.component';
+import { DatepickerComponent } from './datepicker/datepicker.component';
 import { DialogComponent } from './dialog/dialog.component';
 import { OpenDialogComponent } from './dialog/open-dialog/open-dialog.component';
 import { MaterialRoutingModule } from './material-routing.module';
@@ -13,6 +15,7 @@ import { TableComponent } from './table/table.component';
     DialogComponent,
     OpenDialogComponent,
     TableComponent,
+    DatepickerComponent
   ],
   entryComponents: [
     OpenDialogComponent
@@ -20,6 +23,9 @@ import { TableComponent } from './table/table.component';
   imports: [
     SharedModule,
     MaterialRoutingModule
-  ]
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'zn-TW' },
+  ],
 })
 export class MaterialModule { }
