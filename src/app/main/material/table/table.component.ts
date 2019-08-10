bd5c4d5cc38adeaeccf591c4a3188d20b2aa5dc4
slioplark <jeailfy9520@outlook.com>
2019-08-10 10:35:32 +0800

    feat loading

--- src/app/main/material/table/table.component.ts
+++ src/app/main/material/table/table.component.ts
@@ -10,12 +10,14 @@ export class TableComponent implements OnInit {
 
   displayedColumns: string[] = ['id', 'name', 'email', 'phone', 'website', 'management'];
   dataSource: any;
+  loading = true;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.http.get('https://jsonplaceholder.typicode.com/users').subscribe(data => {
       this.dataSource = data;
+      this.loading = false;
     });
   }
 
