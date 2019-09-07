7e1cac14bf473b161375bebd7520eda516eb597e
slioplark <jeailfy9520@outlook.com>
2019-09-08 01:28:06 +0800

    feat MatDialogRef close method

--- src/app/main/material/dialog/open-dialog/open-dialog.component.ts
+++ src/app/main/material/dialog/open-dialog/open-dialog.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-open-dialog',
@@ -8,9 +8,16 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 })
 export class OpenDialogComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<OpenDialogComponent>
+  ) { }
 
   ngOnInit() {
+  }
+
+  cancel() {
+    this.dialogRef.close(false);
   }
 
 }
